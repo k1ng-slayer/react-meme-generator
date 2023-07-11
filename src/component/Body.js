9b5a7ba7b3a56b69c32b7be2d321e15fe8a5d77b
a.mahagaonkar@bm-systems.in <a.mahagaonkar@bm-systems.in>
2023-07-11 10:56:56 +0530

    stage

--- src/component/Body.js
+++ src/component/Body.js
@@ -2,26 +2,57 @@ import React, { useState } from "react";
 import memesData from "../memesData";
 
 function Body() {
-    const [meme, setMeme] = useState("");
+    const [num, setNum] = useState(49);
+    const [text, setText] = useState({
+        top: "",
+        bottom: "",
+    });
+
+    const img = memesData.data.memes[num].url;
+    const len = memesData.data.memes.length;
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setNum(Math.floor(Math.random() * len));
+        console.log(num);
+        console.log(text);
+    };
+
+    const handleChange = (event) => {
+        setText((prevText) => ({
+            ...prevText,
+            [event.target.name]: event.target.value,
+        }));
+    };
 
     return (
         <main>
-            <form className="text=form">
+            <form className="text-form">
                 <input
                     type="text"
                     placeholder="Top text"
-                    className="form-input"
+                    name="top"
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     placeholder="Bottom text"
-                    className="form-input"
+                    name="bottom"
+                    onChange={handleChange}
                 />
-                <button className="form-button">Get a new meme image 🖼</button>
+                <button className="form-button" onClick={handleClick}>
+                    Get a new meme image
+                </button>
             </form>
-            <img src={meme} alt="random-pic" />
+            <div>
+                <img src={img} alt="random-pic" />
+                <div className="text-top">{text.top}</div>
+                <div className="text-bottom">{text.bottom}</div>
+            </div>
         </main>
     );
 }
 
 export default Body;
+
+// https://picsum.photos/640/360
